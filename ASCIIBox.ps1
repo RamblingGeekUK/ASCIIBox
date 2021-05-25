8cabf6454dd50ca4d34bb300612b1e12197e9006
Wayne Taylor <wayne@kryptos.co.uk>
2021-05-25 17:25:09 +0100

    Basic working version of the inital goal, seems to have some bugs to iron out

--- ASCIIBox.ps1
+++ ASCIIBox.ps1
@@ -8,42 +8,29 @@ $HorizontalJoinCharLeft = [string][char]9568
 $HorizontalJoinCharRight = [string][char]9571 #(╣)
 
 Function CreateBoxText() {
-
     param
     (
-        [string[]]$msg,[string]$title
+        [string[]]$msg, [string]$title
     )
-        $maxLength = $Host.UI.RawUI.WindowSize.Width - 2
-        $lineCount = 0
-        $msg -split "`r`n" | ForEach-Object {
-            $text += $msg
-            If ($text[$lineCount].Length -gt $maxLength) {
-                $maxLength = $text[$lineCount].Length
-            }
-            $lineCount++
-        }
-        
-        # Title 
-        #$title = "jfjfdfdjfsdjlkjfjsdkljfklsdlfkljfljsjfklfjkdjfkl"
-
-        $TopLeftBoxChar + ($HorizontalBoxChar * ($maxLength)) + $TopRightBoxChar
-        
+    Begin {}
+    Process {
+        $maxLength = $Host.UI.RawUI.WindowSize.Width - 2  
+    }
+    End {
+        #Title, add if statement here if title is empty draw box around text.
+         $TopLeftBoxChar + ($HorizontalBoxChar * ($maxLength)) + $TopRightBoxChar
         Write-HostCenter -Message $title
-        
         $HorizontalJoinCharLeft + ($HorizontalBoxChar * ($maxLength)) + $HorizontalJoinCharRight
 
         #$TopLeftBoxChar + ($HorizontalBoxChar * ($maxLength)) + $TopRightBoxChar
-        For ($i = 0; $i -lt $lineCount; $i++) {           
-            Write-Host($VerticalBoxChar + " " + $text[$i] + (" " * ($maxLength - $text[$i].Length - 1)) + $VerticalBoxChar)
-            
+        For ($i = 0; $i -lt $msg.Count; $i++) {           
+            $VerticalBoxChar + " " + $msg[$i] + (" " * ($maxLength - $msg[$i].Length -1)) + $VerticalBoxChar
         }
         $BottomLeftBoxChar + ($HorizontalBoxChar * ($maxLength)) + $BottomRightBoxChar
-    }  
-
-
-    
+    }
+}  
 
 function Write-HostCenter { 
     param($Message)
-    Write-Host ("{0}{1}" -f ($VerticalBoxChar + ' ' * (([Math]::Max(0, $Host.UI.RawUI.BufferSize.Width / 2) - [Math]::Floor($Message.Length / 2)))), $Message.PadRight($Host.UI.RawUI.BufferSize.Width / 2 + $Message.Length / 2 - 2)  + $VerticalBoxChar)
+    Write-Host ("{0}{1}" -f ($VerticalBoxChar + ' ' * (([Math]::Max(0, $Host.UI.RawUI.BufferSize.Width / 2) - [Math]::Floor($Message.Length / 2)))), $Message.PadRight($Host.UI.RawUI.BufferSize.Width / 2 + $Message.Length / 2 - 2) + $VerticalBoxChar)
 }
